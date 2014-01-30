60a292c181f0ec225e424a2939b04cfb82ee8b58
Jackson Owens <jackson_owens@brown.edu>
2014-01-29 22:22:42 -0500

    some miley changes

---
+++ miley.js
@@ -0,0 +1,11 @@
+/*
+    miley.js
+    An implementation of the MileyFeed project.
+*/
+
+// This code will be executed when the page finishes loading
+window.addEventListener('load', function(){
+    // Fill me in!
+}, false);
+
+// Add more supporting code here!
